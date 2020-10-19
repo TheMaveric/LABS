8ac8e771cd792a8ae67ed81ae3dc73e6f3a4024f
Kushagra Mishra <kushagramishra98@gmail.com>
2020-10-19 15:13:22 +0530

    Add files via upload

--- NPS/client.c
+++ NPS/client.c
@@ -1,30 +1,63 @@
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
-int main(int argc,char *argv[])
-{
-  int create_socket;
-  int bufsize = 1024, cont;
-  char *buffer = malloc(bufsize);
-  char fname[256];
-  struct sockaddr_in address;
-  if ((create_socket = socket(AF_INET,SOCK_STREAM,0)) > 0)
-  printf("The Socket was created\n");
-  address.sin_family = AF_INET;
-  address.sin_port = htons(15001);
-  inet_pton(AF_INET,argv[1],&address.sin_addr);
-  if (connect(create_socket,(struct sockaddr *) &address, sizeof(address)) == 0)
-  printf("The connection was accepted with the server %s...\n",argv[1]);
-  printf("Enter The Filename to Request : "); scanf("%s",fname);
-  send(create_socket, fname, sizeof(fname), 0);
-  printf("Request Accepted... Receiving File...\n\n");
-  printf("The contents of file are...\n\n");
-  while((cont=recv(create_socket, buffer, bufsize, 0))>0) {
-    write(1, buffer, cont);
-  }
-  printf("\nEOF\n");
-  return close(create_socket);
-}
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    for (;;) { 
+        bzero(buff, sizeof(buff)); 
+        printf("Enter the string : "); 
+        n = 0; 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+        write(sockfd, buff, sizeof(buff)); 
+        bzero(buff, sizeof(buff)); 
+        read(sockfd, buff, sizeof(buff)); 
+        printf("From Server : %s", buff); 
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+  
+int main() 
+{ 
+    int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    servaddr.sin_port = htons(PORT); 
+  
+    // connect the client socket to server socket 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    // function for chat 
+    func(sockfd); 
+  
+    // close the socket 
+    close(sockfd); 
+} 
